21d670b0d3d9cbb27ec9bcbb850da39cb61ccee2
miguel angel romero parra <mr9501459@gmail.com>
2025-07-26 11:49:57 -0500

    feat: gun and new bullets

--- juego_java/Juego1/src/script.js
+++ juego_java/Juego1/src/script.js
@@ -16,6 +16,112 @@ const config = {
   },
 };
 
+const bulletConfigs = {
+  right: {
+    offsetX: 20,
+    offsetY: 10,
+    velocityX: 800,
+    velocityY: 0,
+    flipX: false,
+    rotation: 0,
+  },
+  left: {
+    offsetX: -20,
+    offsetY: 10,
+    velocityX: -800,
+    velocityY: 0,
+    flipX: true,
+    rotation: 0,
+  },
+  lookUpRight: {
+    offsetX: 30,
+    offsetY: -20,
+    velocityX: 800,
+    velocityY: -800,
+    flipX: false,
+    rotation: -45,
+  },
+  lookUpLeft: {
+    offsetX: -30,
+    offsetY: -20,
+    velocityX: -800,
+    velocityY: -800,
+    flipX: true,
+    rotation: 45,
+  },
+  lookUp: {
+    offsetX: 15,
+    offsetY: -30,
+    velocityX: 0,
+    velocityY: -800,
+    flipX: false,
+    rotation: -90,
+  },
+};
+
+const dudeConfigs = {
+  left: {
+    setVelocityX: -160,
+    action: "left",
+    weaponPositionX: -12,
+    weaponPositionY: 10,
+    facing: "left",
+    rotation: 0,
+    flipX: true,
+    visible: true,
+  },
+  right: {
+    setVelocityX: 160,
+    action: "right",
+    weaponPositionX: 12,
+    weaponPositionY: 10,
+    facing: "right",
+    rotation: 0,
+    flipX: false,
+    visible: true,
+  },
+  lookUpLeft: {
+    setVelocityX: -160,
+    action: "lookUpLeft",
+    weaponPositionX: -30,
+    weaponPositionY: -20,
+    facing: "lookUpLeft",
+    rotation: 45,
+    flipX: true,
+    visible: true,
+  },
+  lookUpRight: {
+    setVelocityX: 160,
+    action: "lookUpRight",
+    weaponPositionX: 30,
+    weaponPositionY: -20,
+    facing: "lookUpRight",
+    rotation: -45,
+    flipX: false,
+    visible: true,
+  },
+  lookUp: {
+    setVelocityX: 0,
+    action: "lookUp",
+    weaponPositionX: 15,
+    weaponPositionY: -30,
+    facing: "lookUp",
+    rotation: -90,
+    flipX: false,
+    visible: true,
+  },
+  turn: {
+    setVelocityX: 0,
+    action: "turn",
+    weaponPositionX: 0,
+    weaponPositionY: 0,
+    facing: "turn",
+    rotation: 0,
+    flipX: false,
+    visible: false,
+  },
+};
+
 var game = new Phaser.Game(config);
 
 var player;
@@ -31,6 +137,7 @@ function preload() {
   this.load.image("sky", "assets/Luna pixelart.png");
   this.load.image("ground", "assets/ground.png");
   this.load.image("bullet", "assets/bala.png");
+  this.load.image("gun", "assets/Gun.png");
 
   //dude sprites
   this.load.spritesheet("dude", "assets/dude.png", {
@@ -64,13 +171,17 @@ function create() {
   platforms.create(650, 230, "ground").setScale(1, 0.4).refreshBody();
   platforms.create(100, 400, "ground").setScale(1, 0.4).refreshBody();
   platforms.create(400, 568, "ground").setScale(2, 0.6).refreshBody();
+
+  //player
   player = this.physics.add.sprite(100, 450, "dude", 0);
   player.setScale(0.2);
-
   player.setCollideWorldBounds(true);
   player.setBounce(0.3);
 
-  //animaciones normales
+  //gun
+  this.weapon = this.add.sprite(player.x + 20, player.y - 20, "gun");
+  this.weapon.setVisible(false);
+
   this.anims.create({
     key: "left",
     frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
@@ -93,21 +204,22 @@ function create() {
 
   //----animaciones mirando arriba
   this.anims.create({
-    key: "lookUp",
-    frames: [{ key: "dudeUp", frame: 4 }],
-    frameRate: 20,
-  });
-
-  this.anims.create({
-    key: "lookUpRight",
-    frames: this.anims.generateFrameNumbers("dudeUp", { start: 5, end: 8 }),
+    key: "lookUpLeft",
+    frames: this.anims.generateFrameNumbers("dudeUp", { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1,
   });
 
   this.anims.create({
-    key: "lookUpLeft",
-    frames: this.anims.generateFrameNumbers("dudeUp", { start: 0, end: 3 }),
+    key: "lookUp",
+    frames: [{ key: "dudeUp", frame: 4 }],
+    frameRate: 20,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "lookUpRight",
+    frames: this.anims.generateFrameNumbers("dudeUp", { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1,
   });
@@ -131,8 +243,9 @@ function create() {
 }
 
 function update() {
+  this.weapon.setPosition(player.x, player.y);
+
   if (vidas <= 0) {
-    // Si no quedan vidas, mostrar mensaje de fin de la partida
     this.add
       .text(400, 270, "Fin de la partida", {
         fontSize: "64px",
@@ -143,36 +256,22 @@ function update() {
     return;
   }
 
-  //animaciones mirando arriba
+  // Animaciones y movimiento
   if (this.keyW.isDown && cursors.right.isDown) {
-    player.anims.play("lookUpRight", true);
-    player.setVelocityX(160);
-    return;
+    executeActions(this, "lookUpRight");
   } else if (this.keyW.isDown && cursors.left.isDown) {
-    player.anims.play("lookUpLeft", true);
-    player.setVelocityX(-160);
-    return;
-  } else if (this.keyW.isDown) {
-    player.anims.play("lookUp", true);
-    player.setVelocityX(0);
-    return;
-  }
-
-  //animaciones normales
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play("left", true);
-    player.facing = "left";
+    executeActions(this, "lookUpLeft");
+  } else if (cursors.left.isDown) {
+    executeActions(this, "left");
   } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-    player.anims.play("right", true);
-    player.facing = "right";
+    executeActions(this, "right");
+  } else if (this.keyW.isDown) {
+    executeActions(this, "lookUp");
   } else {
-    player.setVelocityX(0);
-    player.anims.play("turn");
+    executeActions(this, "turn");
   }
 
-  if (this.keyE.isDown) {
+  if (this.keyE.isDown && player.facing !== "turn") {
     const currentTime = this.time.now;
 
     //intervalo de tiempo entre disparo
@@ -188,17 +287,37 @@ function update() {
 }
 
 //-----secondary functions
+
+//dude actions
+function executeActions(scene, action) {
+  const configDude = dudeConfigs[action];
+  if (!configDude) return; // No se ejecuta si no hay configuración para la acción
+
+  player.anims.play(configDude.action, true);
+  player.setVelocityX(configDude.setVelocityX);
+  player.facing = configDude.facing;
+  scene.weapon.setVisible(configDude.visible);
+
+  scene.weapon.setPosition(
+    player.x + configDude.weaponPositionX,
+    player.y + configDude.weaponPositionY
+  );
+  scene.weapon.setFlipX(configDude.flipX);
+  scene.weapon.rotation = Phaser.Math.DegToRad(configDude.rotation);
+}
+
+//bullets
 function shootBullet() {
-  let bulletSpeed = 800;
-  let direction = player.facing === "right" ? 1 : -1;
+  const config = bulletConfigs[player.facing];
+  if (!config) return; // No se dispara si no hay configuración para la dirección
 
-  let bulletX = player.x + (direction + 20);
-  let bulletY = player.y;
+  const bulletX = player.x + config.offsetX;
+  const bulletY = player.y + config.offsetY;
 
-  let bullet = bullets.create(bulletX, bulletY, "bullet");
+  const bullet = bullets.create(bulletX, bulletY, "bullet");
   bullet.body.setAllowGravity(false);
-  bullet.setScale(0.08, 0.1);
-  bullet.setVelocityX(bulletSpeed * direction);
-
-  bullet.setFlipX(direction === -1);
+  bullet.setScale(0.05, 0.1);
+  bullet.setVelocity(config.velocityX, config.velocityY);
+  bullet.setFlipX(config.flipX);
+  bullet.rotation = Phaser.Math.DegToRad(config.rotation);
 }
